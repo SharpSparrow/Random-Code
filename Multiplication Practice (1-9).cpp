994ca99b92da7b4524b4156207586f7ba531bb03
SharpSparrow <80430547+SharpSparrow@users.noreply.github.com>
2021-09-23 11:16:54 -0700

    Update Multiplication Practice (1-9).cpp
    Completed Time Limit!

--- Multiplication Practice (1-9).cpp
+++ Multiplication Practice (1-9).cpp
@@ -1,34 +1,45 @@
 // Still in development //
 // Planned Changes : Timer for time limit //
 #include <iostream>
-
+#include <time.h>
 
 using namespace std;
 
-
+time_t start = time(0);
 int streak;
 int main()
 {
     
+
+
+
+
     int answer;
     int random;
     
     srand((unsigned)time(0));
      random = (rand()%9)+1;
       int random2;
-    
+    double seconds_since_start = difftime( time(0), start);
      random2 = (rand()%9)+1;
-     cout <<random<<" * "<<random2<<" = ";
+     cout <<"("<< seconds_since_start <<")"<<random<<" * "<<random2<<" = ";
      cin >> answer;
+      if (seconds_since_start >= 60) {
+        cout <<"\nToo Late\nYour streak was "<<streak<<"!\n";
+    return 0;
+          
+      }
+    
      if (answer == (random *random2)) {
          streak = streak + 1;
          cout <<"Correct - ( Streak = "<<streak<<")\n";
+        
          main();
          
      } else if (!cin) {
          cout <<"Please choose a valid number";
      } else {
-         cout <<"Incorrect\n";
+         cout <<"Incorrect | The answer was "<<random *random2<<"!\n";
          cout <<"You had a streak of "<<streak<< "!";
      }
 
